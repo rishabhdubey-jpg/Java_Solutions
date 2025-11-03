93ff0c1dc6db3976c8174c2c62ab7241b9683e1f
RISHABH DUBEY <rishabhdx06@gmail.com>
2025-11-03 14:05:07 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+   System.out.println("Hello World!");
+          /* if & else..
+                int a=5;
+                if(a%2==0){
+                    System.out.println("even");
+                }else{
+                    System.out.println("odd");
+                }
+                                                    */
+/* sum of 2 no.'s ..
+        int a=1,b=2,c=3;
+        int sum = a+b+c;
+        System.out.println(sum);   */
+
+/* Typecasting..
+         byte a=5;
+         byte b=6;
+     //  byte c=a+b;   // gives error bcuzz the arthimatic prog of any two no's is a int type..
+         byte c=(byte)(a+b);  // This is correct..
+ System.out.println(c);     */
+
+
+/* loop..
+       for(int i=0;i<5;i++){
+            System.out.println(i); }     */
+
+/* I/O of variables..
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter value of a ");
+        int a= sc.nextInt();
+        System.out.println("Enter value of b ");
+        int b= sc.nextInt();
+        System.out.println(a+b);     */
+
+
+            }
+        }
